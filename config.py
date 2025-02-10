957def7c959b348c53a5e982639da7f9cde0039d
usernamex007 <sonalixsachin@gmail.com>
2025-02-10 11:36:17 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SESSION_STRINGS = os.getenv("SESSION_STRINGS").split(",")  # Multiple Sessions Supported
